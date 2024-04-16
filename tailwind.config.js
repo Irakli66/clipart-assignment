d80044bcb8dd00abc1d482ca6e290081ccfe4e61
irakli66 <xarshiladzeirakli@gmail.com>
2024-04-16 13:45:30 +0400

    project starter

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
